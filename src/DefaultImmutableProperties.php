389b41b5ae603b191b82e06bb16b8b94b33ac174
Arne De Smedt <arnedesmdt@gmail.com>
2021-09-20 10:36:52 +0200

    create defaults for immutable objects
    if the properties of that object have defaults or are immutable objects
    with recursively default child properties

---
+++ src/DefaultImmutableProperties.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\JsonImmutableObjects;
+
+use EventEngine\Data\ImmutableRecord;
+use EventEngine\Data\SpecialKeySupport;
+use InvalidArgumentException;
+use ReflectionClass;
+
+use function preg_match;
+
+trait DefaultImmutableProperties
+{
+    private function init(): void
+    {
+        // phpcs:ignore Squiz.NamingConventions.ValidVariableName.NotCamelCaps
+        foreach (self::$__propTypeMap as $key => [$type, $isNative, $isNullable]) {
+            if ($isNullable || $isNative) {
+                continue;
+            }
+
+            $specialKey = $key;
+
+            if ($this instanceof SpecialKeySupport) {
+                $specialKey = $this->convertKeyForArray($key);
+            }
+
+            if (isset($this->{$specialKey})) {
+                continue;
+            }
+
+            $reflectionType = new ReflectionClass($type);
+
+            if (! $reflectionType->implementsInterface(ImmutableRecord::class)) {
+                continue;
+            }
+
+            try {
+                $this->{$specialKey} = $type::fromArray([]);
+            } catch (InvalidArgumentException $exception) {
+                if (! preg_match('/^Missing record data for key/', $exception->getMessage())) {
+                    throw $exception;
+                }
+            }
+        }
+    }
+}
