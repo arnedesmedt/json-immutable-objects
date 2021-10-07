722536a45efbc1ab280b53f5e2786a6b6fc00fa5
Arne De Smedt <arnedesmdt@gmail.com>
2021-10-07 13:51:11 +0200

    reset useMaxValues

--- src/DefaultImmutableProperties.php
+++ src/DefaultImmutableProperties.php
@@ -42,10 +42,13 @@ trait DefaultImmutableProperties
             try {
                 $this->{$specialKey} = $type::fromArray([], self::$useMaxValues);
             } catch (InvalidArgumentException $exception) {
+                self::$useMaxValues = false;
                 if (! preg_match('/^Missing record data for key/', $exception->getMessage())) {
                     throw $exception;
                 }
             }
+
+            self::$useMaxValues = false;
         }
     }
 }
