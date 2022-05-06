acc0d36802b8de2a6c28d9a14c00666ff8734694
Arne De Smedt <arnedesmdt@gmail.com>
2022-05-06 08:44:10 +0200

    add factory from array

---
+++ src/FactoryFromArray.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\JsonImmutableObjects;
+
+use EventEngine\JsonSchema\JsonSchemaAwareRecord;
+
+trait FactoryFromArray
+{
+    /**
+     * @return class-string<JsonSchemaAwareRecord>
+     */
+    abstract protected static function modelClass(): string;
+
+    /**
+     * @param array<string, mixed> $array
+     */
+    public static function fromArray(array $array): mixed
+    {
+        return self::modelClass()::fromArray($array);
+    }
+}
